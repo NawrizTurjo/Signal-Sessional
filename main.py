4e58c53f8298f4a3db3ccb8032dee340bc66ba13
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2024-09-13 16:59:02 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,200 @@
+# print("hello world")
+
+# a=10
+# if a%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# word = "hello"
+# multi_line = """hello
+
+# world"""
+
+# print(word)
+# print(multi_line)
+
+# comment
+
+"""multi line 
+
+comment"""
+
+# input
+
+# name = input("Enter your name: ")
+# print("Hello", name)
+
+# a = int(input("Enter a number: "))
+# b = input("Enter another number: ")
+
+# print(type(a), type(b))
+
+# print("Hello")
+# print("World")
+
+# print("Hello", end=" ")
+# print("World")
+
+# print("Hello", "World", sep=" ")
+
+#multiple assignment
+
+# a, b, c = 10, 20, 30
+# print(a, b, c)
+
+# a = 10+5j
+# b = 20+5j
+# print(a+b)
+
+# #list
+
+# a = [1, "string", 3, 4, 5]
+# print(a)
+
+# # tuple
+
+# b = (1, 2, 3, 4, 5)
+# print(b)
+
+# a = [10,20,30,40]
+
+# print(a[2:4]) #slicing
+
+# print(a[:-2])
+
+#string concat
+
+# a = "hello"
+# b = "world"
+# print(a+b)
+# print(a*2)
+
+# Dictionarioes
+
+# a = {1: "one", 2: "two", 3: "three"}
+# print(a)
+# print(a[1])
+# if(1 in a):
+#     print("yes")
+
+# d={}
+# d["hehe"] = 10
+# d["haha"] = 20
+# d["haha"] = 30
+# print(d)
+# if "haha" in d:
+#     print("yes")
+# print(d.keys())
+# print(d.values())
+
+# a = 3**2
+# print(a)
+
+# a= 3/2
+# b= 3//2
+# print(a)
+# print(b)
+
+# bin
+
+# a = 10
+# print(bin(a))
+
+# a = [1,2,3,4,5]
+# if 1 in a:
+#     print("yes")
+# else:
+#     print("no")
+
+# x=[1,2,3,4,5]
+# y=[1,2,3,4,5]
+# if x is y:
+#     print("yes")
+# else:
+#     print("no")
+
+# if(cnd1):
+#     ---
+# elif(cnd1):
+#     ---
+# else:
+#     ---
+
+# a=2
+# b=3
+
+# if a>b:
+#     print("a is greater")
+# elif a==b:
+#     print("equal")
+# else:
+#     print("b is greater")
+
+#loops
+
+# for i in range(5):
+#     print(i)
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+
+#functions
+
+# def add(a,b):
+#     return a+b
+
+# print(add(10,20))
+
+# def add(a,b):
+#     return a+b,"a+b",[a,b]
+
+# a,b,c = add(10,20)
+# print(a)
+# print(b)
+# print(c)
+
+# print(add(10,20))
+
+# def printinfo (str):
+#     "This prints a passed string into this function"
+#     print(str)
+#     return
+
+# printinfo("hello world")
+
+# def defArg(a,b=10):
+#     print(a,b)
+#     return a+b
+
+# print(defArg(b=20,a=30))
+
+# def printinfo (arg1, *vartuple):
+#     "This prints a variable passed arguments"
+#     # print(type(vartuple))
+#     print(arg1,end=" ")
+#     for var in vartuple:
+#         print(var,end=" ")
+#     print()
+#     return
+
+# printinfo(10)
+# printinfo(10,20,30,40)
+
+# def printinfo (arg1, **vardict):
+#     "This prints a variable passed arguments"
+#     print(arg1)
+#     print(vardict)
+#     return
+
+# printinfo(10,a=20,b=30)
+
+# from math import sqrt
+# import math
+# import math as mt
+
+# a=math.sqrt(16)
+# print(a)
+
+
---
+++ test.py
@@ -0,0 +1,62 @@
+# # a=10
+# # b=10
+
+# #taking input from user
+# a=int(input("Enter a number: "))
+# b=int(input("Enter another number: "))
+
+# print(a+b)
+# print(type(a))
+
+# def myfun():
+#     print("Hello")
+#     a=15
+#     b=20
+#     print(a+b)
+
+# myfun()
+
+# if a==b:
+#     print("Equal")
+# elif a>b:
+#     print("Greater")
+# else:
+#     print("Lesser")
+
+
+# #string quation mark
+
+# str = 'This is a string'
+# str1 = "This is also a string"
+# str2 = '''This is also
+# a multiple line string'''
+# str3 = """This is also a 
+
+# multiline string"""
+
+# print(str)
+# print(str1)
+# print(str2)
+# print(str3)
+
+# string_a = int(input("Enter a number: "))
+
+# print (type(string_a))
+
+#complex number
+# a=10+5j
+# print(a)
+
+#tuple and list
+
+# a=(1,2,3,4,5)
+# b=[1,2,3,4,5]
+
+# print(a)
+# print(b)
+
+# b[2]=10
+# # a[2]=10
+#     # error: tuple object does not support item assignment
+# # print(a)
+# print(b)
